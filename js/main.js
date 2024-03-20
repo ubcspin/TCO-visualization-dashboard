b315a434e04650ecbb09a2eb9e27109a6d8051a3
Mara Solen <marasolen@gmail.com>
2024-03-19 22:28:08 -0700

    finished 4 views

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@ const charts = {};
 /**
  * Load data from CSV file asynchronously and render charts
  */
-d3.csv('data/comfort-objects.csv').then(data => {
-    console.log(data);
-    
+d3.csv('data/comfort-objects.csv').then(data => {   
     let container = document.getElementById("bar-chart-container");
     charts["bar-chart"] = new BarChart({
         parentElement: "#bar-chart",
@@ -30,7 +28,7 @@ d3.csv('data/comfort-objects.csv').then(data => {
     charts["scatter-plot-general"] = new ScatterPlot({
         parentElement: "#jitter-plot",
         width: container.clientWidth - 20,
-        height: container.clientHeight - 70,
+        height: container.clientHeight - 120,
         yDimension: "Object Category",
         xDimension: "Portability",
         colourDimension: "Gender"
@@ -38,27 +36,40 @@ d3.csv('data/comfort-objects.csv').then(data => {
 
     // charts.push(new SankeyDiagram());
 
-    // charts.push(new WordCloud());
+    container = document.getElementById("word-cloud-container");
+    charts["word-cloud"] = new WordCloud({
+        parentElement: "#word-cloud",
+        width: container.clientWidth - 20,
+        height: container.clientHeight - 70,
+        dimension: "Size"
+    }, JSON.parse(JSON.stringify(data)));
 });
 
-const fillDropdownSelection = (dropdown, options, optionSelector, hierarchySelector) => {
+const fillDropdownSelection = (dropdown, options, optionSelector, hierarchySelector, defaultOption) => {
     options.forEach(o => {
-        const option = document.createElement("option")
-        option.text = option.value = optionSelector(o);
-        dropdown.appendChild(option);
         if (hierarchySelector) {
             hierarchySelector(o).forEach(so => {
                 const option = document.createElement("option")
-                option.text = option.value = "--- " + so;
+                option.text = option.value = "[" + optionSelector(o) + "] " + so;
                 dropdown.appendChild(option);
+                if (defaultOption === so) {
+                    dropdown.value = option.value;
+                }
             });
+        } else {
+            const option = document.createElement("option")
+            option.text = option.value = optionSelector(o);
+            dropdown.appendChild(option);
+            if (defaultOption === o) {
+                dropdown.value = option.value;
+            }
         }
     });
 };
 
 d3.json('data/dimensions.json').then(dimensions => {
     let dropdown = document.getElementById("bar-chart-dimension-selector");
-    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"]);
+    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"], "Gender");
     dropdown.onchange = () => {
         let selection = document.getElementById("bar-chart-dimension-selector").value;
         if (selection.startsWith("--- ")) {
@@ -69,16 +80,52 @@ d3.json('data/dimensions.json').then(dimensions => {
 
         charts["scatter-plot-softness"].colourDimension = selection;
         charts["scatter-plot-softness"].updateVis();
+
+        charts["scatter-plot-general"].colourDimension = selection;
+        charts["scatter-plot-general"].updateVis();
     }
     
     dropdown = document.getElementById("scatter-plot-dimension-selector");
-    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"]);
+    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"], "Age");
     dropdown.onchange = () => {
         let selection = document.getElementById("scatter-plot-dimension-selector").value;
-        if (selection.startsWith("--- ")) {
-            selection = selection.substring(4);
+        if (selection.startsWith("[")) {
+            selection = selection.substring(selection.indexOf("]") + 2);
         }
         charts["scatter-plot-softness"].yDimension = selection;
         charts["scatter-plot-softness"].updateVis();
+    }
+    
+    dropdown = document.getElementById("jitter-plot-dimension-y-selector");
+    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"], "Object Category");
+    dropdown.onchange = () => {
+        let selection = document.getElementById("jitter-plot-dimension-y-selector").value;
+        if (selection.startsWith("[")) {
+            selection = selection.substring(selection.indexOf("]") + 2);
+        }
+        charts["scatter-plot-general"].yDimension = selection;
+        charts["scatter-plot-general"].updateVis();
+    }
+    
+    dropdown = document.getElementById("jitter-plot-dimension-x-selector");
+    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"], "Portability");
+    dropdown.onchange = () => {
+        let selection = document.getElementById("jitter-plot-dimension-x-selector").value;
+        if (selection.startsWith("[")) {
+            selection = selection.substring(selection.indexOf("]") + 2);
+        }
+        charts["scatter-plot-general"].xDimension = selection;
+        charts["scatter-plot-general"].updateVis();
+    }
+    
+    dropdown = document.getElementById("word-cloud-dimension-selector");
+    fillDropdownSelection(dropdown, dimensions, o => o["block"], (o) => o["dimensions"], "Size");
+    dropdown.onchange = () => {
+        let selection = document.getElementById("word-cloud-dimension-selector").value;
+        if (selection.startsWith("[")) {
+            selection = selection.substring(selection.indexOf("]") + 2);
+        }
+        charts["word-cloud"].dimension = selection;
+        charts["word-cloud"].updateVis();
     }
 });
--- js/wordCloud.js
+++ js/wordCloud.js
@@ -1,53 +1,87 @@
 class WordCloud extends Chart {
 
-  /**
-   * Class constructor with initial configuration
-   * @param {Object}
-   */
-  // Todo: Add or remove parameters from the constructor as needed
-  constructor(_config, data) {
-    this.config = {
-      parentElement: _config.parentElement,
-      containerWidth: _config.width,
-      containerHeight: _config.height,
-      margin: {
-        top: 30,
-        right: 5,
-        bottom: 20,
-        left: 30
-      }
-    }
-    this.data = data;
-    this.initVis();
-  }
+	/**
+	 * Class constructor with initial configuration
+	 * @param {Object}
+	 */
+	// Todo: Add or remove parameters from the constructor as needed
+	constructor(_config, data) {
+		super();
+		this.config = {
+		parentElement: _config.parentElement,
+		containerWidth: _config.width,
+		containerHeight: _config.height,
+		margin: {
+			top: 30,
+			right: 5,
+			bottom: 20,
+			left: 30
+		}
+		}
+		this.data = data;
+		this.dimension = _config.dimension;
+		this.initVis();
+	}
 
-  initVis() {
-    let vis = this;
+	initVis() {
+		let vis = this;
 
-    // Calculate inner chart size. Margin specifies the space around the actual chart.
-    vis.config.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-    vis.config.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+		// Calculate inner chart size. Margin specifies the space around the actual chart.
+		vis.config.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+		vis.config.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
-    // Define size of SVG drawing area
-    vis.svg = d3.select(vis.config.parentElement)
-      .attr('width', vis.config.containerWidth)
-      .attr('height', vis.config.containerHeight);
+		// Define size of SVG drawing area
+		vis.svg = d3.select(vis.config.parentElement)
+		.attr('width', vis.config.containerWidth)
+		.attr('height', vis.config.containerHeight);
 
-    // Append group element that will contain our actual chart
-    // and position it according to the given margin config
-    vis.chartArea = vis.svg.append('g')
-      .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+		// Append group element that will contain our actual chart
+		// and position it according to the given margin config
+		vis.chartArea = vis.svg.append('g')
+		.attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
-    // Todo: initialize scales, axes, static elements, etc.
-  }
+		vis.sizeScale = d3.scaleLinear()
+			.range([12, 40]);
 
-  updateVis() {
-    let vis = this;
-    // Todo: Prepare data and scales
-  }
+		vis.layout = d3.layout.cloud()
+			.size([vis.config.width, vis.config.height])
+			.fontSize(d => vis.sizeScale(d.size))
+			.rotate(0)
+			.padding(10);
 
-  renderVis() {
-    let vis = this;
-    // Todo: Bind data to visual elements, update axes
-  }
+		vis.updateVis();
+	}
+
+	updateVis() {
+		let vis = this;
+
+		vis.rollupData = d3.rollups(vis.data, d => d.length, d => d[vis.dimension]);
+		vis.rollupData = vis.rollupData.map(d => { return { text: d[0], size: d[1] } });
+		console.log(this.rollupData);
+
+		vis.sizeScale.domain(d3.extent(vis.rollupData.map(d => d.size)));
+
+		vis.layout.words(vis.rollupData);
+
+		vis.renderVis();
+	}
+
+	renderVis() {
+		let vis = this;
+
+		const draw = (words) => {
+			vis.chartArea.selectAll(".word")
+				.data(words)
+				.join("text")
+				.attr("class", "word")
+				.attr("font-size", d => d.size)
+				.attr("text-anchor", "middle")
+				.attr("transform", d => {
+					return "translate(" + [d.x + vis.config.width / 2, d.y + vis.config.height / 2] + ")";
+				})
+				.text(d => d.text);
+		};
+
+		vis.layout.on("end", draw).start();
+	}
 }
